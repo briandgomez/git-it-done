d4ef92e249ab427823e788e03628eaa473199663
briandgomez <briandgomez98@gmail.com>
2021-09-21 19:35:28 -0400

    query parameters and handle errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,10 +1,26 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
+var getRepoName = function () {
+    //grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        //display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else {
+        //if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+}
 
 var getRepoIssues = function (repo) {
-    console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    //make a get request to url
     fetch(apiUrl).then(function (response) {
         //request was successful
         if (response.ok) {
@@ -19,7 +35,8 @@ var getRepoIssues = function (repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            //if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     })
 };
@@ -75,4 +92,4 @@ var displayWarning = function (repo) {
     limitWarningEl.appendChild(linkEl);
 }
 
-getRepoIssues("angular/angular");
+getRepoName();
